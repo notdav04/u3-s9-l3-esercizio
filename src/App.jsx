6bb3084aff4997319ad22f8d8c8c52f702fbb081
notdav04 <davide.alonzi26@gmail.com>
2024-11-27 15:51:35 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+// import AllTheBooks from "./components/AllTheBooks";
+import MyFooter from "./components/MyFooter";
+import MyNav from "./components/MyNav";
+import Welcome from "./components/Welcome";
+
+import BookList from "./components/BookList";
+import fantasy from "./data/fantasy.json";
+
+function App() {
+  return (
+    <div className="App d-flex flex-column">
+      <div>
+        <MyNav />
+        <Welcome />
+        {/* <AllTheBooks /> */}
+
+        <BookList list={fantasy} />
+
+        <MyFooter />
+      </div>
+    </div>
+  );
+}
+
+export default App;
